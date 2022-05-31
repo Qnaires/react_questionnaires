91e9886ccd2f7539c397162f9e47784aad5b5261
Dio-Dao <99-989@mail.ru>
2022-05-31 14:59:12 +0300

    init

---
+++ client/src/api/index.tsx
@@ -0,0 +1,22 @@
+let baseUrl;
+
+export const proxyUrl = 'https://express-questionnaires-server.herokuapp.com';
+
+
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = '/api';
+}
+if (process.env.NODE_ENV === 'production') {
+  baseUrl = `${proxyUrl}/api`;
+}
+
+export const PATHS = {
+  login: `${baseUrl}/auth/login`,
+  signup: `${baseUrl}/auth/signup`,
+  refresh: `${baseUrl}/auth/refresh`,
+  poll: `${baseUrl}/poll`,
+  template: `${baseUrl}/template`,
+  user: `${baseUrl}/auth/getAllUsers`,
+  result: `${baseUrl}/result`,
+  statistic: `${baseUrl}/result/statistic`,
+};
---
+++ client/webpack/webpack.dev.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'https://express-questionnaires-server.herokuapp.com',
+                secure: false,
+                changeOrigin: true,
+            },
+        },
+        client: {
+            logging: 'info',
+            overlay: false,
+        },
+    },
+    devtool: 'cheap-module-source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.name': JSON.stringify('dev'),
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+        }),
+    ],
+};
